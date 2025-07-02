c5b89108afaf95a3b90c93cc2085671333bba57f
kicheol83 <suyikim7@gmail.com>
2025-07-02 18:43:01 +0900

    feat: orginize and test pages router system

---
+++ pages/community/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useState } from "react";
+const Community = () => {
+  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+  const [title, setTitle] = useState<string>("hello");
+  return (
+    <div>
+      COMMUNITY{" "}
+      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+        Press Me
+      </button>
+    </div>
+  );
+};
+
+export default Community;
