3b6ff6c7675fe9edbc822872cd9b091ff49d4403
kicheol83 <suyikim7@gmail.com>
2025-07-07 17:16:32 +0900

    feat: property list publishing

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,11 +1,48 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Stack } from "@mui/material";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
 import { NextPage } from "next";
+import { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Filter from "@/libs/components/property/Filter";
+import PropertyCard from "@/libs/components/property/PropertyCard";
 
 const PropertyList: NextPage = () => {
+  const [properties, setProperties] = useState<number[]>([1, 2, 3, 4, 5, 6]);
   return (
-    <div style={{ margin: "20px 0" }}>
-      <Stack className="container">PROPERTY LIST</Stack>
+    <div id="property-list-page" style={{ position: "relative" }}>
+      <Stack className="container">
+        <Box className="right">
+          <span>Sort by</span>
+          <div>
+            <Button endIcon={<KeyboardArrowDownIcon />}>New</Button>
+          </div>
+        </Box>
+        <Stack className="property-page">
+          <Stack className="filter-config">
+            <Filter />
+          </Stack>
+          <Stack className="main-config" mb={"76px"}>
+            <Stack className="list-config">
+              {properties.map((property, index) => {
+                return <PropertyCard key={index} />;
+              })}
+            </Stack>
+            <Stack className="pagination-config">
+              <Stack className="pagination-box">
+                <Pagination
+                  page={1}
+                  count={5}
+                  shape="circular"
+                  color="primary"
+                />
+              </Stack>
+              <Stack className="total-result">
+                <Typography>Total 5 properties available</Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
     </div>
   );
 };
